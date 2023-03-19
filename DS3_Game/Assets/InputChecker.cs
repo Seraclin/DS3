45147da4f5026283160e48c8c036f94629c1b03d
Samantha Lin <sampster8@gmail.com>
2023-03-19 17:26:49 -0400

    Update InputChecker.cs
    Added a script to read key inputs and print them to the console.

--- DS3_Game/Assets/InputChecker.cs
+++ DS3_Game/Assets/InputChecker.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * This script is used to detect what keys were pressed and print to the Console.
+ * */
 public class InputChecker : MonoBehaviour
 {
     // Start is called before the first frame update
